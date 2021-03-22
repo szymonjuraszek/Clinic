5cc7eb673123dd64ce331c5c93336a760c49356f
Szymon Juraszek <szymonjuraszek777@gmail.com>
2021-03-22 16:18:24 +0100

    Correct updating typeahead component

--- Znany-Lekarz-FrontEnd/src/app/home/specialization-field/specialization-field.component.ts
+++ Znany-Lekarz-FrontEnd/src/app/home/specialization-field/specialization-field.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {Component, EventEmitter, Input, Output, ViewChild} from '@angular/core';
 import {merge, Observable, Subject} from "rxjs";
 import {debounceTime, distinctUntilChanged, filter, map} from "rxjs/operators";
 import {NgbTypeahead} from "@ng-bootstrap/ng-bootstrap";
@@ -10,14 +10,15 @@ import {NgbTypeahead} from "@ng-bootstrap/ng-bootstrap";
 })
 export class SpecializationFieldComponent {
 
-  @Input() doctorSpecializations: Array<String>;
+  @Input() doctorSpecializations: Array<string>;
 
   @Output() specializationEvent = new EventEmitter<string>();
 
-  // @ts-ignore
   @ViewChild('instance', {static: true}) instance: NgbTypeahead;
   focus$ = new Subject<string>();
   click$ = new Subject<string>();
+
+  model: any;
 
   search = (text$: Observable<string>) => {
     const debouncedText$ = text$.pipe(debounceTime(200), distinctUntilChanged());
@@ -28,19 +29,5 @@ export class SpecializationFieldComponent {
       map(term => (term === '' ? this.doctorSpecializations
         : this.doctorSpecializations.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1)).slice(0, 10))
     );
-  }
-
-  onKey(event) {
-    this.specializationEvent.emit(event.target.value);
-  }
-
-  onFocus(event) {
-    this.focus$.next(event.target.value);
-    this.specializationEvent.emit(event.target.value);
-  }
-
-  onClick(event) {
-    this.click$.next(event.target.value);
-    this.specializationEvent.emit(event.target.value);
   }
 }
